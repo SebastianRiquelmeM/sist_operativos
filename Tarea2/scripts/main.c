4a5ffad09dd1f4496a54f3345048a892ef9d4f99
Sebastian Riquelme <sebarsalbo@gmail.com>
2022-05-18 19:32:26 -0400

    a

--- Tarea2/scripts/main.c
+++ Tarea2/scripts/main.c
@@ -1,58 +1,61 @@
-#include <stdio.h>
-#include <semaphore.h>
+#include<semaphore.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<pthread.h>
+sem_t x,y;
+pthread_t tid;
+pthread_t writerthreads[100],readerthreads[100];
+int readercount = 0;
 
-typedef struct _rwlock_t{
-	sem_t lock;   // semaforo binario bloquear 
-	sem_t writelock; // semaforo para permitir solo un escritor o muchos lectores
-	int readers;     // cuantos lectores estan accediendo c:
-} rwlock_t;
-
-void rwlock_init(rwlock_t *rw){ //inicializacion
-	rw->readers =0;     // cero lectoresa
-	sem_init(&rw->lock,0,1);  
-	sem_init(&rw->writelock,0,1);  // semaforos verde y rojo 
-}
-
-void rwlock_acquire_readlock(rwlock_t *rw){ // adquirir lectura 
-	sem_wait(&rw->lock);   // espera el acceso exclusivo
-	rw->readers++;         // auemnta el numero de lectores
-    printf("readers: %d",rw->readers);
-	if (rw->readers ==1)   
-		sem_wait(&rw->writelock);  // con esto cuando alguien lee bloquea al que escribe
-	sem_post(&rw->lock);  // cuando finaliza se finaliza el acceso al lector
-
-}
-
-void rwlock_release_readlock(rwlock_t *rw){   // sale region critica
-	sem_wait(&rw->lock);  // adquire el bloqueo 
-	rw->readers--;        // disminuye lectores
-	if(rw->readers == 0)  
-		sem_post(&rw->writelock);  // señaliza al escritor quie puede acceder
-	sem_post(&rw->writelock);      
-}
-
-void rwlock_acquire_writelock(rwlock_t *rw){  // cuando entra el escritor pregunta la señal si esta libre
-	sem_wait(&rw->writelock);
-}
-
-void rwlock_release_writelock(rwlock_t *rw){  //cuando se libera del escritor...
-	sem_post(&rw->writelock);
-}
-
-int main() {
-
-    struct _rwlock_t rwlock1;
-
-    rwlock1.readers=5;
-    rwlock_init(&rwlock1);
-    for(int i=0;i<5;i++){
-        rwlock_acquire_readlock(&rwlock1);
-        printf("\n");
+void *reader(void* param)
+{
+    sem_wait(&x);
+    readercount++;
+    if(readercount==1)
+        sem_wait(&y);
+    sem_post(&x);
+    printf("%d reader is inside\n",readercount);
+    usleep(3);
+    sem_wait(&x);
+    readercount--;
+    if(readercount==0)
+    {
+        sem_post(&y);
     }
-    
+    sem_post(&x);
+    printf("%d Reader is leaving\n",readercount+1);
+    return NULL;
+}
 
+void *writer(void* param)
+{
+    printf("Writer is trying to enter\n");
+    sem_wait(&y);
+    printf("Writer has entered\n");
+    sem_post(&y);
+    printf("Writer is leaving\n");
+    return NULL;
+}
 
-    printf("\nHello, World!\n");
-    return 0;
+int main()
+{
+    int n2,i;
+    printf("Enter the number of readers:");
+    scanf("%d",&n2);
+    printf("\n");
+    int n1[n2];
+    sem_init(&x,0,1);
+    sem_init(&y,0,1);
+    for(i=0;i<n2;i++)
+    {
+        pthread_create(&writerthreads[i],NULL,reader,NULL);
+        pthread_create(&readerthreads[i],NULL,writer,NULL);
+    }
+    for(i=0;i<n2;i++)
+    {
+        pthread_join(writerthreads[i],NULL);
+        pthread_join(readerthreads[i],NULL);
+    }
 
 }
